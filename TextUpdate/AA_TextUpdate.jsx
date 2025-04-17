05d520ab0b41e32db6dbd54bba9877183c28c7bf
ApollosAnimation <ApollosAnimation@gmail.com>
2025-04-17 13:05:17 -0600

    Changing Text replacement script to be able to use a csv file for referencing fonts, as well as using special characters to define when those fonts are used

--- TextUpdate/AA_TextUpdate.jsx
+++ TextUpdate/AA_TextUpdate.jsx
@@ -5,23 +5,25 @@ function aT(){//quick alertTest function for debugging
     }
 
 
-function updateText(textCsvFile, nameCsvFile){// updating text based on CSV files that contain comp names, layer names, and the new Text and Text that is to be italicized
+function updateText(textCsvFile, nameCsvFile, fontCsvFile){// updating text based on CSV files that contain comp names, layer names, and the new Text and Text that is to be italicized
     app.beginUndoGroup("UpdateText");
-    
+    //aT();
     var textCSV = findFootage(textCsvFile).file;
     var textArray = parseCSV(textCSV);
     var nameCSV = findFootage(nameCsvFile).file;
     var nameArray = parseCSV(nameCSV);
+    var fontCSV = findFootage(fontCsvFile).file;
+    var fontArray = parseCSV(fontCSV);
     var textArrayLen = textArray[0].length - 1;    
-    for(var i=0; i< textArray.length; i++){
+    for(var i=1; i< textArray.length; i++){
          for(var j=1; j<=textArrayLen; j++){
             var compName = nameArray[i][0];//Comp name is the first column of the current row in the Legend CSV
             var layerName = nameArray[i][j];//Layer name is the coordinates i,j in the Legend CSV
             var layerText = textArray[i][j];//the new text  is the coordinates i,j in the TextCSV, in the same position of the Layer name in a second file
-            var italicizeString = textArray[i][0];//the strings to find and italicize in the new text string, found in the first column of the current row, and seperated by semicolons
-            var fontPostName = "Roboto-Bold"// Base Text Font
-            var italicizedFontPostName = "Roboto-BoldItalic"// Italics Text Font
-            updateTextLayer(compName, layerName, layerText, fontPostName, italicizeString, italicizedFontPostName)//for every item in the new text array, update the layer with the new text and italicize the given string
+            //var italicizeString = textArray[i][0];//the strings to find and italicize in the new text string, found in the first column of the current row, and seperated by semicolons
+            //var fontPostName = fontArray[0].toString();// Base Text Font
+            //var italicizedFontPostName =  fontArray[1].toString();// Italics Text Font
+            updateTextLayer(compName, layerName, layerText, fontArray)//for every item in the new text array, update the layer with the new text and italicize the given string
             }
         }
     
@@ -187,7 +189,7 @@ function findLayer(comp, layerName)//looking for a specific Layer in a comp by n
 	}
 }
 
-function updateTextLayer(compName, layerName, newTextValue, fontPostName, italicizeString, italicizedFontPostName)//find the text layer to update 
+function updateTextLayer(compName, layerName, newTextValue, fontArray)//find the text layer to update 
 {
     var foundComp = findComp(compName);
     if((foundComp) !=null ){
@@ -196,28 +198,116 @@ function updateTextLayer(compName, layerName, newTextValue, fontPostName, italic
             var layerSourceText = foundLayer.property("Source Text");
             if(layerSourceText != null){
                 layerSourceText.expressionEnabled = false;
-                layerSourceText.setValue(newTextValue);
-                italicizeText(italicizeString, layerSourceText, fontPostName, italicizedFontPostName)
+                var newLayerTextDocument = layerSourceText.value;
+                newLayerTextDocument.text = newTextValue.split(/\\n/g).join("\n");
+                newLayerTextDocument.font = fontArray[1][0].toString();
+                var wordChangeArray = []
+                for(i=1; i<=fontArray[2].length-1; i++){
+                     var newTextValueString = newLayerTextDocument.text;
+                     var searchSymbol = fontArray[2][i];
+                     if (searchSymbol == "\"") {searchSymbol =new RegExp ("\“|\”|\"|\"", 'g'); }
+                        //var newTextValueArray = newTextValueString.split(fontArray[2][i]);
+                        var newTextValueArray = newTextValueString.split(searchSymbol);
+                        if (newTextValueArray.length > 1){
+                        var newTextValueString = newTextValueArray.join("");
+                        newLayerTextDocument.text = newTextValueString;
+                         if (newTextValueArray.length > 1){
+                             for (j=1; j<newTextValueArray.length; j+=2){
+                                 if (newTextValueArray[j] !=""){
+                                wordChangeArray.push([newTextValueArray[j], i]);
+                                }
+                            }
+                         }
+                    }
+                /***
+                for(i=1; i<=fontArray[2].length-1; i++){
+                        var newTextValueString = newLayerTextDocument.text;
+                        var newTextValueArray = newTextValueString.split(fontArray[2][i]);
+                        if (newTextValueArray.length > 2){
+                            var newTextValueString = newTextValueArray.join("");
+                            newLayerTextDocument.text = newTextValueString;
+                            var startPoint = newTextValueArray[0].length;
+                            var endPoint = startPoint + newTextValueArray[1].length;
+                            var fontCharacterRange = newLayerTextDocument.characterRange(startPoint, endPoint);
+                            fontCharacterRange.font = fontArray[1][i];
+                            }
+                            ***/
+                    }
+                //alert(wordChangeArray);
+                layerSourceText.setValue(newLayerTextDocument);
+                if (wordChangeArray.length > 0){
+                    for (i=0; i<wordChangeArray.length;i++){
+                        //alert(wordChangeArray[i]);
+                        var regex = new RegExp (wordChangeArray[i][0],"g")
+                        var searchArray;
+                        while ((searchArray = regex.exec(layerSourceText.value.text)) !== null) {
+                            var endPoint = regex.lastIndex;
+                            var wordLength = wordChangeArray[i][0];
+                            var startPoint = endPoint - wordChangeArray[i][0].length;
+                            //var fontCharacterRange = layerSourceText.value.characterRange(startPoint, endPoint);
+                            var newFont = fontArray[1][wordChangeArray[i][1]];
+                            //fontCharacterRange.font = newFont;
+                            newLayerTextDocument.characterRange(startPoint, endPoint).font = newFont;
+                        }
+                    }
+                }
+                //italicizeText(italicizeString, layerSourceText, fontPostName, italicizedFontPostName)
+                layerSourceText.setValue(newLayerTextDocument);
             }
         }
     }
 }
 
-
 function italicizeText(italicizeString, textDocument, fontPostName, italicizedFontPostName)//Given the string we want to italicize and the text document using Character Range, while also setting the default font
 {
+    var updatedTextDoc = textDocument;
     var italicizeArray = italicizeString.split(/\;\s?/);
+    var layerString = updatedTextDoc.value;
+    layerString.font = fontPostName
     for (var i = 0; i<italicizeArray.length; i++){
-        var layerString = textDocument.value;
-        layerString.font = fontPostName
-        var startPoint = layerString.text.search(italicizeArray[i]);
-        if(startPoint == -1){continue};
-        var endPoint = startPoint + italicizeArray[i].length;
-        var titleCharacterRange = layerString.characterRange(startPoint, endPoint);
-        titleCharacterRange.font = italicizedFontPostName;
-        textDocument.setValue(layerString);
+        //var startPoint = layerString.text.search(italicizeArray[i]);
+        var regex = new RegExp (italicizeArray[i],"g")
+        //var searchArray = layerString.text.match(regex);
+        var searchArray;
+        while ((searchArray = regex.exec(layerString)) !== null) {
+            var endPoint = regex.lastIndex;
+            var startPoint = endPoint - italicizeArray[i].length;
+            var titleCharacterRange = layerString.characterRange(startPoint, endPoint);
+            titleCharacterRange.font = italicizedFontPostName;
+            var titleCharacterRange = layerString.characterRange(startPoint, endPoint);
+            titleCharacterRange.font = italicizedFontPostName;
+            }
+        //alert(searchArray[1]);
+        //if(startPoint == -1){continue};
+        //var endPoint = startPoint + italicizeArray[i].length;
+        
+        //reflectAll (titleCharacterRange);
+        //reflectAll (updatedTextDoc);
+        //updatedTextDoc.setValue(layerString);
     }
+    updatedTextDoc.setValue(layerString);
 }
+
+function parseText()//this is a function to edit strings to include returns
+{
+        var layerGroup = app.project.activeItem.selectedLayers;
+        //reflectAll(layerGroup[0].sourceText.value.fontObject.postScriptName);
+        for (i=0; i<layerGroup.length; i++){
+            //alert(layerGroup[i].sourceText.value.fontObject.postScriptName);
+            reflectAll(layerGroup[i].sourceText);
+            }
+        }
+
+function getFontName()//this is a function to identify font names to be put in a CSV
+{
+        var layerGroup = app.project.activeItem.selectedLayers;
+        //reflectAll(layerGroup[0].sourceText.value.fontObject.postScriptName);
+        for (i=0; i<layerGroup.length; i++){
+            alert(layerGroup[i].sourceText.value.fontObject.postScriptName);
+            //var inspect = layerGroup[i].sourceText.value;
+            //alert(inspect.text);
+            }
+        }
 
 function reflectAll(reflectable)//this is a debugging function that lists all information about a given element, and is a great break point to analyze in Extendscript
 {
@@ -243,7 +333,10 @@ function makeEffectsPanel(windowObj){//creating a window for the buttons and tex
          var textCsvFile = textReplaceColumn01 .add('edittext{text:"TextCSV", justify:"center", active:true, enterKeySignalsOnChange:flase}');
          textReplaceColumn01 .add('statictext {text: "CSV Legend Name:",  justify: "center"}');
          var legendCsvFile = textReplaceColumn01 .add('edittext{text:"LegendCSV", justify:"center", active:true, enterKeySignalsOnChange:flase}');
-         textReplaceColumn01 .add("Button",undefined ,"Update Text").onClick = function(){updateText(textCsvFile.text, legendCsvFile.text)};
+         textReplaceColumn01 .add('statictext {text: "CSV Font Name:",  justify: "center"}');
+         var fontCsvFile = textReplaceColumn01 .add('edittext{text:"FontCSV", justify:"center", active:true, enterKeySignalsOnChange:flase}');
+         textReplaceColumn01 .add("Button",undefined ,"Update Text").onClick = function(){updateText(textCsvFile.text, legendCsvFile.text,fontCsvFile.text)};
+         textReplaceColumn01 .add("Button",undefined ,"Get Font Name").onClick = function(){getFontName()};
         myEffectsPanel.layout.layout(true);
         
             
